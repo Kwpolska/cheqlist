cf72169b4e8a708f7196b8916378e2e094c92aa4
Chris Warrick <kwpolska@gmail.com>
2022-01-01 00:00:00 +0000

    2022! :fireworks:

--- cheqlist/pastewindow.py
+++ cheqlist/pastewindow.py
@@ -1,13 +1,13 @@
 # -*- encoding: utf-8 -*-
 # Cheqlist v0.3.2
 # A simple Qt checklist.
-# Copyright © 2015-2021, Chris Warrick.
+# Copyright © 2015-2022, Chris Warrick.
 # See /LICENSE for licensing information.
 
 """
 The Cheqlist app.
 
-:Copyright: © 2015-2021, Chris Warrick.
+:Copyright: © 2015-2022, Chris Warrick.
 :License: BSD (see /LICENSE).
 """
 
